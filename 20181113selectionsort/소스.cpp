dddec2c075cdc343fd8c7e51461eb88d6e063544
2016440129 <worhkd963@gmail.com>
2018-11-20 14:37:42 +0900

    '20181120'

--- 20181113selectionsort/소스.cpp
+++ 20181113selectionsort/소스.cpp
@@ -13,17 +13,17 @@ void printArray(int *arr, int len){ //앞으로 배열을 함수에 전달할 경우 이 문법을
 //함수 : findMinIndex()
 //입력 : 배열(배열이름, 배열길이)
 //출력 : 최소값의 인덱스
-int findMinIndex(int *arr, int length){
-	int minIndex = 0;
+int findMinIndex(int *arr, int length, int start){
+	int minIndex = start;
 	int count;
-	for(count = 1; count<length; count++){
+	for(count = start+1; count<length; count++){
 		if(arr[minIndex] > arr[count])
 			minIndex = count;
 	}
 	return minIndex;
 }
 
-//함수 : findeMin()
+//함수 : findMin()
 //입력 : 배열(배열이름, 배열길이)
 //출력 : 배열 최소값
 int findMin(int *arr, int length){
@@ -52,14 +52,18 @@ void swapElement(int *arr, int i, int j){
 //출력 : 없음
 //부수효과 : 배열이 정렬됨
 void selectionSort(int *arr, int length){
-	
-	
+	int start;
+	int indexMin;
+	for(start = 0; start<length-1; start++){
+		indexMin = findMinIndex(arr, length, start);
+		swapElement(arr, start, indexMin);
+	}
 }
 int main(){	
 	int a[] = {30, 35, 27, 15, 40}; 
 
 	printf("minimum value of the array is %d\n", findMin(a,5));
-	printf("The index of the minimum value is %d\n", findMinIndex(a,5));
+	//printf("The index of the minimum value is %d\n", findMinIndex(a,5));
 	swapElement(a, 0, 3);
 	printArray(a, 5);
 	
